aa09a08bc6f22625e4d0da4ce08819eabdc0d1f4
Your Name <you@example.com>
2025-02-15 10:34:13 +0500

    manging data basse

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// if (localStorage.getItem("sb-tsiriyarbapweplseeqv-auth-token")) {
-//  console.log("user is log in");
+if (localStorage.getItem("sb-tsiriyarbapweplseeqv-auth-token")) {
+ console.log("user is log in");
  
-//     window.location.href = "./Link-up Ui/index.html";
-// } else {
-// console.log("no usr found");
-//     window.location.href = "./User Creationals/user.html";
-//    }
-// //    localStorage.clear()
+    window.location.href = "./Link-up Ui/index.html";
+} else {
+console.log("no usr found");
+    window.location.href = "./User Creationals/user.html";
+   }
+//    localStorage.clear()
