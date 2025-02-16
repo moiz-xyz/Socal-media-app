27ca9ffca30234c240c18ca9902ea26307cb4997
Your Name <you@example.com>
2025-02-16 09:35:18 +0500

    completed

--- Link-up Ui/ui.js
+++ Link-up Ui/ui.js
@@ -38,12 +38,12 @@ upload.addEventListener("click", async (event) => {
   //   getPublicUrl(fileName);
   // }
 
-  
 
-  let storedProfile = JSON.parse(localStorage.getItem("profile")) || {};
-  if (storedProfile.image) {
-    document.getElementById("profile-pic").src = storedProfile.image;
-  }
+
+  // let storedProfile = JSON.parse(localStorage.getItem("profile")) || {};
+  // if (storedProfile.image) {
+  //   document.getElementById("profile-pic").src = storedProfile.image;
+  // }
   let storedUsers = JSON.parse(localStorage.getItem("users")) || [];
   let username_of_user = storedUsers.map((user) => user.username);
   let name_of_user = storedUsers.map((user) => user.name);
